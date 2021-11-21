c365fbc65fc0016158c9c62f19682f079a242655
Eric Kitaif <eric.kitaif@gmail.com>
2021-11-21 17:22:58 -0500

    Data in rows for prediction

--- times.R
+++ times.R
@@ -7,7 +7,16 @@ mutate_times <- function(x) {
   
   result <- x %>% 
     mutate(day = day(ymd_hms(time)),
-           hour = hour(ymd_hms(time)))
+           hour = hour(ymd_hms(time)),
+           month = month(ymd_hms(time), label = TRUE),
+           year = year(ymd_hms(time)),
+           weekday = wday(ymd_hms(time), label = TRUE),
+           yearday = yday(ymd_hms(time)),
+           # hour_quarter = case_when(minute(ymd_hms(time)) < 15 ~ 1,
+           #                          minute(ymd_hms(time)) < 30 ~ 2,
+           #                          minute(ymd_hms(time)) < 45 ~ 3,
+           #                          TRUE ~ 4)
+           )
   
   return(result)
 }
