3f0ad5c684d46a30ba86ff98444e1f9562e97a24
Eric Kitaif <eric.kitaif@gmail.com>
2021-12-05 11:16:03 -0500

    Incorporated wider dataset

--- README.md
+++ README.md
@@ -8,11 +8,7 @@ This is for a group project for POL706 at Georgetown University
 
 This could graph weather verses departures, temperature vs departues, heat maps, and the other examples in `visualizations.Rmd`.
 
-2. Widen hour_data on the column type for each station. 
-
-This should produce two new columns to replace the column for each station. `example.Rmd` contains where to do this. For example `23rd_&_e_st_nw` should become `23rd_&_e_st_nw_dep` and `23rd_&_e_st_nw_arr` based on the `type` field. The other columns are all the same value for both rows (departure and arrival row for each station) and should be unchanged.
-
-This will require a minor update for ridership when complete.
+~~2. Widen hour_data on the column type for each station.~~
 
 3. Clean up weather predictors.
 
--- manipulate_data.R
+++ manipulate_data.R
@@ -63,12 +63,12 @@ get_station_hourly <- function(x) {
   hour_data_temp <- add_grouping_times(x) %>%
     group_by(year, month, day, hour, station, type) %>%
     summarize(count = n()) %>%
+    # Make departure and arrival plural as it is now a count
+    mutate(type = ifelse(type == "departure", "departures", "arrivals")) %>% 
     # Each station has its own row, this puts all the stations on one row
-    pivot_wider(names_from = "station", values_from = "count") %>%
-    # Renames the type to make it plural since it is a count now
-    mutate(type = ifelse(type == "departure", "departures", "arrivals")) %>%
+    pivot_wider(names_from = c("station", "type"), values_from = "count") %>% 
     # Replace NAs with 0
-    mutate_all(~ ifelse(is.na(.), 0, .)) %>%
+    mutate_all(~ ifelse(is.na(.), 0, .)) %>% 
     # There are a few Nov 2021 in the data that should not be there, probably returns
     #  I did not get October 31st weather for 2021. I can get it later if we need it.
     filter(!(year == 2021 & (month == "Oct" | month == "Nov")))
@@ -88,8 +88,8 @@ setup_for_time_prediction <- function(x, station_name) {
 
   # Create a new field called ridership that will be the actual riders on the
   # given station
-  ridership_data <- x %>%
-    mutate(ridership = NA)
+  lagged_data <- x %>%
+    mutate(departures = NA, arrivals = NA)
 
   # This loop gets the values from the given station and puts it into the ridership
   # ridership column 14 days into the future. The goal is to couple the given station rides
@@ -100,42 +100,29 @@ setup_for_time_prediction <- function(x, station_name) {
     for (month in month.abb) {
       for (day in 1:31) {
         for (hour in 0:23) {
-          dep_index_prev <- which(hour_data$year == year &
+          # find the row for the given hour
+          index_prev <- which(hour_data$year == year &
             hour_data$month == month &
             hour_data$day == day &
-            hour_data$hour == hour &
-            hour_data$type == "departures")
+            hour_data$hour == hour)
 
-          if (length(dep_index_prev) > 0) {
+          # If that row exists (it should)...
+          if (length(index_prev) > 0) {
             future_time <- make_datetime(year, match(month, month.abb), day, hour) + days(14)
-            dep_index_future <- which(hour_data$year == year(future_time) &
+            
+            # See if there is a row 14 days in the future, the last 14 days will not have one
+            index_future <- which(hour_data$year == year(future_time) &
               hour_data$month == (month.abb[month(future_time)]) &
               hour_data$day == day(future_time) &
-              hour_data$hour == hour(future_time) &
-              hour_data$type == "departures")
-
-            if (length(dep_index_future) > 0) {
-              # Get future ridership and pair it with 14 day ago rides.
-              ridership_data$ridership[dep_index_prev] <- ridership_data[[station_name]][dep_index_future]
-            }
-          }
-
-          arr_index_prev <- which(hour_data$year == year &
-            hour_data$month == month &
-            hour_data$day == day &
-            hour_data$hour == hour &
-            hour_data$type == "arrivals")
-
-          if (length(arr_index_prev) > 0) {
-            future_time <- make_datetime(year, match(month, month.abb), day, hour) + days(14)
-            arr_index_future <- which(hour_data$year == year(future_time) &
-              hour_data$month == (month.abb[month(future_time)]) &
-              hour_data$day == day(future_time) &
-              hour_data$hour == hour(future_time) &
-              hour_data$type == "arrivals")
-
-            if (length(arr_index_future) > 0) {
-              ridership_data$ridership[arr_index_prev] <- ridership_data[[station_name]][arr_index_future]
+              hour_data$hour == hour(future_time))
+            
+            # If that future row is there...
+            if (length(index_future) > 0) {
+              # Get future departures and arrivals and pair it with 14 day ago rides.
+              dep_column_name <- paste(station_name, "departures", sep = "_")
+              arr_column_name <- paste(station_name, "arrivals", sep = "_")
+              lagged_data$departures[index_prev] <- lagged_data[[dep_column_name]][index_future]
+              lagged_data$arrivals[index_prev] <- lagged_data[[arr_column_name]][index_future]
             }
           }
         }
@@ -145,21 +132,21 @@ setup_for_time_prediction <- function(x, station_name) {
 
   # Shift the dates 14 days into the future
   # I tried to do this with a map function, but ran into issues.
-  for (index in 1:nrow(ridership_data)) {
+  for (index in 1:nrow(lagged_data)) {
     date <- make_datetime(
-      ridership_data$year[index],
-      match(ridership_data$month[index], month.abb),
-      ridership_data$day[index]
+      lagged_data$year[index],
+      match(lagged_data$month[index], month.abb),
+      lagged_data$day[index]
     )
 
     future_date <- date + days(14)
 
-    ridership_data$year[index] <- year(future_date)
-    ridership_data$month[index] <- month.abb[month(future_date)]
-    ridership_data$day[index] <- day(future_date)
+    lagged_data$year[index] <- year(future_date)
+    lagged_data$month[index] <- month.abb[month(future_date)]
+    lagged_data$day[index] <- day(future_date)
   }
 
-  return(ridership_data)
+  return(lagged_data)
 }
 
 #' Adds weather to data grouped by hour
